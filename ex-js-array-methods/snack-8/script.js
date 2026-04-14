22db9b4a4dd42d8ae8758f963a7773f80442033b
giulia-buonasperanza <giulia.9402@gmail.com>
2026-04-14 17:10:20 +0200

    snack8

--- ex-js-array-methods/snack-8/script.js
+++ ex-js-array-methods/snack-8/script.js
@@ -21,3 +21,13 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+
+const  classeStudente = students.find (function (student){
+  return student.name === 'Marco Lanci'
+} ) 
+
+console.log (classeStudente.class);
+
+//Trovo l’oggetto con .find() (usando il nome come condizione)
+// Poi accedo alla proprietà .class dell’oggetto trovato
