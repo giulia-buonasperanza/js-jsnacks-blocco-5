b038e640585819fc0ee9c5d0ff6cdca3f429f990
giulia-buonasperanza <giulia.9402@gmail.com>
2026-04-14 15:57:30 +0200

    snack1

--- ex-js-array-methods/snack-1/script.js
+++ ex-js-array-methods/snack-1/script.js
@@ -2,3 +2,12 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+//forEach() mi sembra la soluzione più semplice.
+names.forEach(function (name) {
+    console.log(name);
+});
+
+//ma poi ho pensato anche alla join()
+console.log(names.join(","));
